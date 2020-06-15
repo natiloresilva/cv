028a5f02285b4918659b4573dbaf94e8f2ae419b
Natalia Silva <natalia.pupushi@gmail.com>
2020-06-15 13:00:33 +0200

    first commit

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+
+$para = 'natalialorenasilva@hotmail.com';
+
+$asunto= "Mensaje desde Mi Web";
+$mailheader = "From: ".$_POST["email"]."\r\n";
+$mailheader = "Reply-To: ".$_POST["email"]."\r\n";
+$mailheader = "Content-type: text/html; charset=utf-8\r\n";
+$MESSAGE_BODY = "Nombre: ".$_POST["name"]."\n";
+$MESSAGE_BODY = "\n<br>Email: ".$_POST["email"]."\n";
+$MESSAGE_BODY = "\n<br>Mensaje: .n12br($_POST["message"])."\n";
+
+
+
+mail($para, $asunto, $ MESSAGE_BODY, $mailheader); or die ("Error al enviar email!);
+
+header('Location: http://jojotavm.com/sonia')
+?>
